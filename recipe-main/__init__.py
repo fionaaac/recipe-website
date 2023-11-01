8c8d08ce324edda8727ab027cdd67312be46aa50
fionaaac <cchiu2@andrew.cmu.edu>
2023-11-01 13:41:09 +0100

    added venv files to run the app and changed the SQLALCHEMY_DATABASE_URI to use sqlite

--- recipe-main/__init__.py
+++ recipe-main/__init__.py
@@ -10,9 +10,10 @@ def create_app(test_config=None):
 # A secret for signing session cookies
     app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
 
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = "mysql+mysqldb://recipewebsite:hello@localhost/RecipeWebsite"
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://recipewebsite:hello@localhost/RecipeWebsite"
+    
+    # this one runs on sqlite which we can use until we figure out db
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipe-main.db"
 
     # Register blueprints
     # (we import main from here to avoid circular imports in the next lab)
