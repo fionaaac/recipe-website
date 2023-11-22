67b052c1e0f68859bb7190460092945f9ae47b41
fionaaac <cchiu2@andrew.cmu.edu>
2023-11-22 15:51:53 +0100

    login functionality added

--- recipe-main/main.py
+++ recipe-main/main.py
@@ -2,6 +2,7 @@ import datetime
 import dateutil.tz
 
 from flask import Blueprint, render_template
+import flask_login
 
 
 from . import model
@@ -10,6 +11,7 @@ bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
+@flask_login.login_required
 def index():
     # user = model.User(1, "mary@example.com", "mary")
     # return render_template("main/index.html", posts=posts)
