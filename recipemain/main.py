144974d9a38e57f8acb7e05a4dc691c6fbbd2c68
fionaaac <cchiu2@andrew.cmu.edu>
2023-12-12 21:36:58 +0100

    implemented bookmarking and unbookmarking feature with basic design

--- recipemain/main.py
+++ recipemain/main.py
@@ -123,14 +123,14 @@ def user(username):
 def saved_recipes():
 
     saved_recipes = model.Recipe.query.filter(model.Recipe.is_saved == True).all()
-    for recipe in saved_recipes:
-        print(f"Recipe ID: {recipe.id}, Title: {recipe.title}, Description: {recipe.description}")
+    # for recipe in saved_recipes:
+    #     print(f"Recipe ID: {recipe.id}, Title: {recipe.title}, Description: {recipe.description}")
     return render_template("main/saved_recipes.html", recipes=saved_recipes)
 
 @bp.route('/recipe/<string:recipe_id>', methods=["POST"])
+@flask_login.login_required
 def save_recipe_post(recipe_id):
 
-    # recipe_id = request.form.get('save')
     if recipe_id is not None:
         recipe = model.Recipe.query.filter_by(id=str(recipe_id)).first()
         if recipe:
@@ -139,53 +139,20 @@ def save_recipe_post(recipe_id):
             print('switched flag!!')
         else:
             abort(400, f"Recipe with id {recipe_id} not found")
-
-    # all_recipes = model.Recipe.query.all()
-    # # Prints all recipes in the database
-    # for recipe in all_recipes:
-    #     print(f"Recipe ID: {recipe.id}, Title: {recipe.title}, Saved: {recipe.is_saved}")
     
     return render_template("main/recipe_info.html", recipe=recipe)
-        
 
-    # recipe = model.Recipe(
-    #     user = flask_login.current_user,
-    #     title = title,
-    #     description = description,
-    #     persons = persons,
-    #     time = time, 
-    # )
-
-    # uploaded_file = request.files['photo']
-    # if uploaded_file.filename != '':
-    #     content_type = uploaded_file.content_type
-    #     if content_type == "image/png":
-    #         file_extension = "png"
-    #     elif content_type == "image/jpeg":
-    #         file_extension = "jpg"
-    #     else:
-    #         abort(400, f"Unsupported file type {content_type}")
-
-    #     photo = model.Photo (
-    #         user=flask_login.current_user,
-    #         recipe=recipe,
-    #         file_extension=file_extension
-    #     )
-
-    #     db.session.add(photo)
-    # db.session.add(recipe)
-    # db.session.commit()
-
-    # path = (
-    #     pathlib.Path(current_app.root_path)
-    #     / "static"
-    #     / "photos"
-    #     / f"photo-{photo.id}.{file_extension}"
-    # )
-    # uploaded_file.save(path)
-
-    # all_recipes = model.Recipe.query.all()
-    # # Prints all recipes in the database
-    # for recipe in all_recipes:
-    #     print(f"Recipe ID: {recipe.id}, Title: {recipe.title}, Description: {recipe.description}")
-    # return redirect(url_for("main.my_recipes"))
+@flask_login.login_required
+@bp.route('/saved-recipes/<string:recipe_id>', methods=["POST"])
+def saved_recipe_delete(recipe_id):
+    if recipe_id is not None:
+        recipe = model.Recipe.query.filter_by(id=str(recipe_id)).first()
+        if recipe:
+            recipe.is_saved = False
+            db.session.commit()
+            print('switched flag!!')
+        else:
+            abort(400, f"Recipe with id {recipe_id} not found")
+    
+    saved_recipes = model.Recipe.query.filter(model.Recipe.is_saved == True).all()
+    return render_template("main/saved_recipes.html", recipes=saved_recipes)
