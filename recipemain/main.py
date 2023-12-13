f2858cbffeedb1c546bab0c3b20d07336a2bb3f2
fionaaac <cchiu2@andrew.cmu.edu>
2023-12-13 19:29:03 +0100

    rating frontend data passing done

--- recipemain/main.py
+++ recipemain/main.py
@@ -198,14 +198,11 @@ def saved_recipe_delete(recipe_id):
 
 @bp.route('/add-rating/<string:recipe_id>', methods=['POST'])
 def submit_rating_post(recipe_id):
-    print("hiiiiii")
-    recipe = model.Recipe.query.filter_by(id=str(recipe_id)).first()
-    if recipe:
-        # Here, 'recipe' holds the recipe object fetched from the database
-        print(recipe.title)
-        for ingredient in recipe.q_ingredients:
-            print(ingredient.quantity, ingredient.units, ingredient.ingredient.name)
-        return render_template("main/recipe_info.html", recipe=recipe)
     selected_star = request.form.get('star')
     print(f"The user selected {selected_star} stars!")
-    # return f"The user selected {selected_star} stars!"
+    recipe = model.Recipe.query.filter_by(id=str(recipe_id)).first()
+    if recipe:
+        print(recipe.title)
+
+        # need to add the rating to the recipe as an object
+        return render_template("main/recipe_info.html", recipe=recipe)
